3127efda5f19e1f8bb9de423e177978272e098b4
yangxiepeng <yangxiepeng@aliyun.com>
2026-01-08 13:57:49 +0800

    修复了 isaacsim/import_z1/__init__.py，添加了 from .import_z1 import Extension 来正确导出扩展类

---
+++ exts/isaacsim.import_z1/isaacsim/import_z1/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Copyright (c) 2023-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Unitree Z1 Import Extension for Isaac Sim
+This extension provides URDF import and RMPflow control for the Unitree Z1 robotic arm.
+"""
+
+from .import_z1 import Extension
